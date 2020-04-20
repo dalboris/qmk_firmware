3725937fc35a1f5cef7351d7292e35baf7f8140d
Boris Dalstein <dalboris@gmail.com>
2020-04-20 16:10:36 +0200

    Move two ASCII art together for easier printing

--- keyboards/lily58/keymaps/boris/keymap.c
+++ keyboards/lily58/keymaps/boris/keymap.c
@@ -35,7 +35,8 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+/* Base Layer + Lower
+ *
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | -  _ |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -48,16 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   | Gui  | Alt  | Ctrl | / Lower /       \Space \  |Ctrl  | Alt  |AltGr |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
- */
-
- [_QWERTY] = LAYOUT( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
-  KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DEL,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                       KC_LGUI, KC_LALT, KC_LCTL, LOWER,           KC_SPC,  KC_RCTL, KC_LALT, KC_RALT                          \
-),
-/* LOWER
+ *
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | F11  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -71,6 +63,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
+
+ [_QWERTY] = LAYOUT( \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
+  KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DEL,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                       KC_LGUI, KC_LALT, KC_LCTL, LOWER,           KC_SPC,  KC_RCTL, KC_LALT, KC_RALT                          \
+),
+/* LOWER
+ */
 [_LOWER] = LAYOUT( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
   _______, KC_HOME, LEFT_DK, KC_UP,   RGHT_DK, KC_END,                   KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC, KC_PIPE, \
@@ -78,7 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_INS,  KC_PSCR, KC_SPC,  KC_ENT,  KC_PGDN, RESET,     KC_0, KC_1,    KC_2,    KC_3,    KC_LCBR, KC_RCBR, _______, \
                        _______, _______, _______, _______,           KC_ENT, _______, _______, _______                         \
 ),
+
+
 /* RAISE
+ *
+ * Note: this layer is currently unused, but we keep the code for potential future use.
+ *
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
