827c3ec68911047a18db078533f915fe478523ff
Boris Dalstein <dalboris@gmail.com>
2020-04-20 15:44:02 +0200

    Symmetric Ctrl/Alt/Shift modifiers + easier desktop switching

--- keyboards/lily58/keymaps/boris/keymap.c
+++ keyboards/lily58/keymaps/boris/keymap.c
@@ -29,6 +29,9 @@ enum custom_keycodes {
   ADJUST,
 };
 
+// Easily switch between desktops on Linux
+#define LEFT_DK C(A(KC_LEFT))
+#define RGHT_DK C(A(KC_RGHT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -40,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | `  ~ |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | '  " |
  * |------+------+------+------+------+------|  Del  |    | Back  |------+------+------+------+------+------|
- * | Ctrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shift |
+ * |Shift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shift |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | Alt  | Gui  | Shift| / Lower /       \Space \  |Ctrl+ |Shift+|AltGr |
- *                   |      |      |      |/       /         \      \ | Alt  | Alt  |      |
+ *                   | Gui  | Alt  | Ctrl | / Lower /       \Space \  |Ctrl  | Alt  |AltGr |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -51,18 +54,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
   KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DEL,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, \
-                       KC_LALT, KC_LGUI, KC_LSFT, LOWER,         KC_SPC, C(KC_LALT), S(KC_LALT), KC_RALT                       \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DEL,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                       KC_LGUI, KC_LALT, KC_LCTL, LOWER,           KC_SPC,  KC_RCTL, KC_LALT, KC_RALT                          \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | F11  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | Ins  | Home |  Up  | End  | PgUp |                    |   7  |  8   |  9   |   [  |   ]  |  |   |
+ * |      | Home |LeftDk|  Up  |RghtDk| End  |                    |   7  |  8   |  9   |   [  |   ]  |  |   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ^  | Back | Left | Down | Right| PgDn |-------.    ,-------|   4  |  5   |  6   |   (  |   )  |  \   |
+ * |   ^  | Back | Left | Down |Right | PgUp |-------.    ,-------|   4  |  5   |  6   |   (  |   )  |  \   |
  * |------+------+------+------+------+------| Reset |    |   0   |------+------+------+------+------+------|
- * |      | PrSc |      |Space |Enter |      |-------|    |-------|   1  |  2   |  3   |   {  |   }  |      |
+ * |      | Ins  | PrSc |Space |Enter | PgDn |-------|    |-------|   1  |  2   |  3   |   {  |   }  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \Enter \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -70,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                  KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC, KC_PIPE, \
-  KC_CIRC, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                  KC_4,    KC_5,    KC_6,    KC_LPRN, KC_RPRN, KC_BSLS, \
-  _______, KC_PSCR, _______, KC_SPC,  KC_ENT,  _______, RESET,     KC_0, KC_1,    KC_2,    KC_3,    KC_LCBR, KC_RCBR, _______, \
+  _______, KC_HOME, LEFT_DK, KC_UP,   RGHT_DK, KC_END,                   KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC, KC_PIPE, \
+  KC_CIRC, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,                  KC_4,    KC_5,    KC_6,    KC_LPRN, KC_RPRN, KC_BSLS, \
+  _______, KC_INS,  KC_PSCR, KC_SPC,  KC_ENT,  KC_PGDN, RESET,     KC_0, KC_1,    KC_2,    KC_3,    KC_LCBR, KC_RCBR, _______, \
                        _______, _______, _______, _______,           KC_ENT, _______, _______, _______                         \
 ),
 /* RAISE
